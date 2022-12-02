ff7b57dad0369958685d564f1fdd6ff84a724916
DanielSanCo <daniel.santos3210@outlook.com>
2022-12-02 12:28:28 -0300

    Erro no cadastro e login - consertar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 import Apresentacao from './pages/Apresentacao';
 import About from './pages/About';
-import Login from './pages/Login';
+import Login from './pages/login/Login';
 import Navbar from './components/NavBar';
+import CadastroUsuario from './pages/CadastroUsuario';
 import Contact from './pages/Contact';
 
 function App() {
@@ -15,10 +13,11 @@ function App() {
     <Router>
     <Navbar/>
       <Routes>
-        <Route path='/' element={<Apresentacao />}/>
-        <Route path='/sobre' element={<About />}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/sobre' element={<About />} />
+        <Route path='/' element={<Apresentacao />} />
+        <Route path='/cadastrousuario' element={<CadastroUsuario />} />
         <Route path='/contato' element={<Contact />}/>
-        <Route path='/login' element={<Login />}/>
       </Routes>
     </Router>
   );
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,9 +1,20 @@
-import axios from 'axios';
-	export const api = axios.create({
-		baseURL: 'https://generatedu-0fmt.onrender.com/swagger'
-	})
+import axios from 'axios'
 
-	export const login = async({url, dados, setDados}: any) => {
-		const resposta = await api.post(url, dados)
-			setDados(resposta.data)
-	}
+export const api = axios.create({
+    baseURL: 'https://generatedu-0fmt.onrender.com/swagger'
+})
+
+export const cadastroUsuario = async(url:any, dados:any, setDado:any) => {
+    const resposta = await api.post(url, dados)
+        setDado(resposta.data)
+}
+
+export const login = async(url:any, dados:any, setDado:any) => {
+    const resposta = await api.post(url, dados)
+        setDado(resposta.data.token)
+}
+
+export const busca = async(url:any, setDado:any, header: any) => {
+    const resposta = await api.get(url, header)
+        setDado(resposta.data)
+}
